8dc32b36f665d243a7c221296aa7e71cacf67d2f
Yoni <yoniakabecky@gmail.com>
2022-09-03 22:30:41 +0900

    install swr
    ps-id: 04858f65-e295-425d-ba9b-6508c61a4564

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import { CacheProvider, EmotionCache, ThemeProvider } from '@emotion/react';
 import CssBaseline from '@mui/material/CssBaseline';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import { SWRConfig } from 'swr';
 
 import createEmotionCache from 'styles/createEmotionCache';
 import { theme } from 'styles/theme';
+import { fetcher } from 'utils/apiFetcher';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -16,12 +18,14 @@ function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <RecoilRoot>
-      <CacheProvider value={emotionCache}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </CacheProvider>
+      <SWRConfig value={{ fetcher }}>
+        <CacheProvider value={emotionCache}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </CacheProvider>
+      </SWRConfig>
     </RecoilRoot>
   );
 }
